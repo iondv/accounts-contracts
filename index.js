80303fc85026f0d7cbc3fcea17e6342d2515d9f8
Dan Krasilnikov <krasilneg@yandex.ru>
2021-02-05 12:48:55 +1000

    v2.0.0

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.AccountStorage = require('./contracts/AccountStorage');
+module.exports.ProfilePlugin = require('./contracts/ProfilePlugin');
+module.exports.StrategyProvider = require('./contracts/StrategyProvider');
